736264eff4abd4aa5c8a4729cdcfd2888fc86114
Yash Hemnani <yash@gxpress.com>
2026-05-13 13:01:57 +0530

    Updated code to fix push worker bug

--- src/providers/push/index.ts
+++ src/providers/push/index.ts
@@ -3,19 +3,12 @@ import webpush from "./webpush";
 export async function sendPush(
   subscription: any,
   payload: object
-) {
-  try {
-    await webpush.sendNotification(
-      {
-        endpoint: subscription.endpoint,
-        keys: subscription.keys,
-      },
-      JSON.stringify(payload)
-    );
-
-    return { success: true };
-  } catch (err) {
-    console.error("Push failed:", err);
-    return { success: false };
-  }
+): Promise<void> {
+  await webpush.sendNotification(
+    {
+      endpoint: subscription.endpoint,
+      keys: subscription.keys,
+    },
+    JSON.stringify(payload)
+  );
 }
